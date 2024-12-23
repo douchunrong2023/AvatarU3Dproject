036fd41ea9b11194060fb8e922f746e504867996
douchunrong2023 <1056637080@qq.com>
2024-12-23 12:34:36 +0800

    1

---
+++ Library/Il2cppBuildCache/iOS/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,59 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void ChangeLabel::changeLabel(System.String)
+extern void ChangeLabel_changeLabel_mCA16AD02DCC29001A0BABBC50CB3ECD6E9141C5B (void);
+// 0x00000002 System.Void ChangeLabel::iOSLog(System.String)
+extern void ChangeLabel_iOSLog_m4805F4AEF97F6708E363C8BC7F197D1940A817F7 (void);
+// 0x00000003 System.Void ChangeLabel::btnClick()
+extern void ChangeLabel_btnClick_m21AE21925E1AD62EED75F2E6A7AF32CF85D2321B (void);
+// 0x00000004 System.Void ChangeLabel::.ctor()
+extern void ChangeLabel__ctor_mD7CC22A2D1D255CA556BA78668150CC99D01D8F2 (void);
+static Il2CppMethodPointer s_methodPointers[4] = 
+{
+	ChangeLabel_changeLabel_mCA16AD02DCC29001A0BABBC50CB3ECD6E9141C5B,
+	ChangeLabel_iOSLog_m4805F4AEF97F6708E363C8BC7F197D1940A817F7,
+	ChangeLabel_btnClick_m21AE21925E1AD62EED75F2E6A7AF32CF85D2321B,
+	ChangeLabel__ctor_mD7CC22A2D1D255CA556BA78668150CC99D01D8F2,
+};
+static const int32_t s_InvokerIndices[4] = 
+{
+	950,
+	1800,
+	1109,
+	1109,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	4,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
